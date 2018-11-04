100118437c660e12bb02d9377a8321c2de095c07
Jiwoo Jang <jiwooo.jang@gmail.com>
2018-11-03 23:51:23 -0400

    Added basic room joining and callbacks

--- Assets/Scripts/Networking/NetworkManager.cs
+++ Assets/Scripts/Networking/NetworkManager.cs
@@ -1,13 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using KickDive.Utility;
 using Photon;
+using Photon.Realtime;
+using Hashtable = ExitGames.Client.Photon.Hashtable;
 
 namespace Photon.Pun {
     public class NetworkManager : MonoBehaviourPunCallbacks {
 
-        public static NetworkManager instance;
+        public enum PlayerNumber {
+            None,
+            // Player 1 starts on the right side of the screen
+            Player1, 
+            // PLayer 2 starts on the left side of the screen
+            Player2,
+        }
 
+        [SerializeField]
+        private string player1RoomPropertyKey, player2RoomPropertyKey;
+
+        public static NetworkManager instance;
+        public static PlayerNumber playerNumber = PlayerNumber.None;
+
+        [SerializeField]
+        private string _playerPrefabName;
         private string _gameVersion = "1.0";
 
         public override void OnEnable() {
@@ -40,6 +57,55 @@ namespace Photon.Pun {
 
         public override void OnConnectedToMaster() {
             Debug.Log("Connected to Photon master server");
+        }
+
+        // Joining a room
+        public void JoinOrCreateRoom(string roomName) {
+            RoomOptions roomOptions = new RoomOptions();
+            roomOptions.IsVisible = false;
+            roomOptions.MaxPlayers = 2;
+            PhotonNetwork.JoinOrCreateRoom(roomName, roomOptions, TypedLobby.Default);
+        }
+
+        public override void OnJoinRoomFailed(short returnCode, string message) {
+            Debug.LogError("Failed to join room");
+            base.OnJoinRoomFailed(returnCode, message);
+        }
+
+        public override void OnJoinedRoom() {
+            base.OnJoinedRoom();
+
+            Debug.Log("Connected to room: " + PhotonNetwork.CurrentRoom.Name);
+            Hashtable setValue = new Hashtable();
+            Hashtable expectedValue = new Hashtable();
+
+            // First person to join gets P1
+            // TODO: Make this a selectable option?
+            if (PhotonNetwork.CurrentRoom.PlayerCount != 0) {
+                playerNumber = PlayerNumber.Player2;
+
+                setValue.Add(player2RoomPropertyKey, PhotonNetwork.LocalPlayer.UserId);
+
+                expectedValue.Add(player2RoomPropertyKey, PhotonNetwork.LocalPlayer.UserId);
+            } else {
+                playerNumber = PlayerNumber.Player1;
+
+                setValue.Add(player1RoomPropertyKey, PhotonNetwork.LocalPlayer.UserId);
+
+                expectedValue.Add(player1RoomPropertyKey, PhotonNetwork.LocalPlayer.UserId);
+            }
+
+            PhotonNetwork.CurrentRoom.SetCustomProperties(setValue, expectedValue);
+            GameUtility.SetPlayerSpawn(playerNumber);
+        }
+
+        public void InstantiatePlayerPrefab() {
+            if (PhotonNetwork.CurrentRoom == null) {
+                Debug.LogError("Cannot instantiate player prefab, not connected to room. Bailing");
+                return;
+            }
+
+            PhotonNetwork.Instantiate(_playerPrefabName, GameUtility.playerSpawn.position, GameUtility.playerSpawn.rotation);
         }
     }
 }
